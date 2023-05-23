43da190f6c8d52711c866a47d79664ea6f2adef2
makarov009 <prottoy.r.ahmed@gmail.com>
2023-05-23 20:02:52 +0600

    signup page functionality added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import SignUp from "./pages/signUp.jsx";
+import { QueryClientProvider, QueryClient } from "react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Routes>
         <Route path="/sign-up" element={<SignUp />} />
       </Routes>
-    </>
+    </QueryClientProvider>
   );
 }
 
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,9 +1,41 @@
-import React from "react";
+import React,{useState} from "react";
+import { useMutation } from "react-query";
 import signUpLogo from "../assets/signup-logo.png";
 import signUpBG from "../assets/signup-bg3.jpg";
 import Button from "../components/Button.jsx";
+import axios from "axios";
 
 function SignUp() {
+
+    const [userData,setUserData] = useState({
+        username:"",
+        fullname:"",
+        email:"",
+        password:"",
+        passwordConfirmation:""
+    });
+const signupMutation = useMutation((userData) =>
+  axios.post("http://localhost:4001/api/v1/auth/sign-up", userData)
+);
+      const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (userData.password !== userData.confirmPassword) {
+        console.log("password didn't match.")
+        }
+        
+        signupMutation.mutate(userData);
+      };
+
+
+        const handleChange = (e) => {
+          const { name, value } = e.target;
+          console.log(name, value)
+          setUserData((prevUserData) => ({
+            ...prevUserData,
+            [name]: value,
+          }));
+        };
   return (
     <section className="bg-white">
       <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
@@ -25,8 +57,12 @@ function SignUp() {
           aria-label="Main"
           className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6"
         >
-          <img className="h-8 sm:h-24" alt="logo" src={signUpLogo} />
           <div className="max-w-xl lg:max-w-3xl">
+            <img
+              className="h-8 sm:h-24 hidden lg:block"
+              alt="logo"
+              src={signUpLogo}
+            />
             <div className="relative -mt-16 block lg:hidden">
               <img className="h-24 mt-10 sm:h-24" alt="logo" src={signUpLogo} />
               <h1 className="mt-1 text-2xl font-kalam font-bold text-gray-900 sm:text-3xl md:text-4xl">
@@ -34,7 +70,10 @@ function SignUp() {
               </h1>
             </div>
 
-            <form action="#" className="mt-8 grid grid-cols-6 gap-6">
+            <form
+              onSubmit={handleSubmit}
+              className="mt-8 grid grid-cols-6 gap-6"
+            >
               <div className="col-span-6 sm:col-span-3">
                 <label for="username" className="labelField">
                   Username
@@ -46,6 +85,8 @@ function SignUp() {
                   name="username"
                   className="inputField"
                   autoComplete="off"
+                  value={userData.username}
+                  onChange={handleChange}
                 />
               </div>
 
@@ -60,6 +101,8 @@ function SignUp() {
                   name="fullname"
                   className="inputField"
                   autoComplete="off"
+                  value={userData.fullname}
+                  onChange={handleChange}
                 />
               </div>
 
@@ -74,6 +117,8 @@ function SignUp() {
                   name="email"
                   className="inputField"
                   autoComplete="off"
+                  value={userData.email}
+                  onChange={handleChange}
                 />
               </div>
 
@@ -88,6 +133,8 @@ function SignUp() {
                   name="password"
                   className="inputField"
                   autoComplete="off"
+                  value={userData.password}
+                  onChange={handleChange}
                 />
               </div>
 
@@ -102,17 +149,18 @@ function SignUp() {
                   name="passwordConfirmation"
                   className="inputField"
                   autoComplete="off"
+                  value={userData.confirmPassword}
+                  onChange={handleChange}
                 />
               </div>
 
               <div className="col-span-6 sm:flex sm:items-center sm:gap-4">
                 <Button
                   text="Sign Up"
-                  onClick={() => {
-                    console.log("pokat");
-                  }}
                 />
+                
 
+                
                 <p className="mt-4 text-md text-gray-500 sm:mt-0">
                   Already have an account?
                   <a href="#" className=" text-lg ml-3 text-sky-700 underline">
