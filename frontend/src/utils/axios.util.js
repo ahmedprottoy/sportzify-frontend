74e1ae03e02e4bb4f484ec5f54371e3353d9ae34
Rumman Ahmed Prottoy <53553949+makarov009@users.noreply.github.com>
2023-06-12 18:17:33 +0600

    env axios added.

--- frontend/src/utils/axios.util.js
+++ frontend/src/utils/axios.util.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:4001/api/",
+  baseURL: import.meta.env.VITE_SERVER_URL,
   withCredentials: true,
 });
 
