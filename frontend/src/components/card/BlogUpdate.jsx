03088b1272eb89aafcd7448b64eed24e0631c442
Rumman Ahmed Prottoy <53553949+makarov009@users.noreply.github.com>
2023-06-14 17:03:14 +0600

    naming changed a little bit

--- frontend/src/components/card/BlogUpdate.jsx
+++ frontend/src/components/card/BlogUpdate.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useBlogData } from "../../hooks/useBlogData";
-import ImageUpload from "../create/imageUpload";
-import CreateInput from "../create/CreateInput";
+import ImageUpload from "../articleCreate/imageUpload";
+import CreateInput from "../articleCreate/CreateInput";
 import { updateBlogReq } from "../../services/blogService";
 import { useMutation,useQueryClient } from "react-query";
 import ButtonUI from "../common/ButtonUI";
